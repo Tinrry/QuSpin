189d18ff906108f4b34c4ac367946daa736e1512
zhh <zhenghuanhuan@zhejianglab.com>
2022-07-05 14:56:45 +0800

    chevbyshev_approximation

--- examples/notebooks/chevbyshev_approximation_fermion_spectral_function.py
+++ examples/notebooks/chevbyshev_approximation_fermion_spectral_function.py
@@ -15,11 +15,12 @@ import scipy.sparse as sp
 
 from quspin.basis import spinful_fermion_basis_general
 from quspin.operators import hamiltonian
+from examples.notebooks.spectral_function_fermion import spectral_function_fermion
 
 
-def zero_temperature_Green_function(omegas, params):
-    print("Mock Green imaginary function.")
-    return 1 / 3 * omegas ** 3 + 2 * omegas ** 2 + omegas - 10
+# def zero_temperature_Green_function(omegas, params):
+#     print("Mock Green imaginary function.")
+#     return 1 / 3 * omegas ** 3 + 2 * omegas ** 2 + omegas - 10
 
 
 # def f(x):
@@ -44,56 +45,57 @@ def scale_down(x, x_min, x_max):
     return (2 * (x - x_min) / (x_max - x_min)) - 1
 
 
-paras = [1.5205, 0.2055, 2.0274, 3.4384, 1.137, 1.2329, 1.4658, 0.4657]
-x_min = -1
-x_max = 3
-x_grid = np.linspace(x_min, x_max, 100)
-plt.figure()
-plt.plot(x_grid, zero_temperature_Green_function(x_grid, paras))
-plt.show()
-plt.close()
 
-n = 2  # order (degree, highest power) of the approximating polynomial
-m = 3  # number of Chebyshev nodes (having m > n doesn't matter for the approximation it seems)
+# n = 112  # order (degree, highest power) of the approximating polynomial
+# m = 113  # number of Chebyshev nodes (having m > n doesn't matter for the approximation it seems)
+for n in [4, 12, 100]:
+    m = n + 1
+    r_k = np.polynomial.chebyshev.chebpts1(m)
 
-r_k = np.polynomial.chebyshev.chebpts1(m)
+    # builds the Vandermonde matrix of Chebyshev polynomial expansion at the r_k nodes
+    # using the recurrence relation
+    T = np.zeros((m, n + 1))
 
-# builds the Vandermonde matrix of Chebyshev polynomial expansion at the r_k nodes
-# using the recurrence relation
-T = np.zeros((m, n + 1))
+    T[:, 0] = np.ones((m, 1)).T
 
-T[:, 0] = np.ones((m, 1)).T
+    T[:, 1] = r_k.T
 
-T[:, 1] = r_k.T
+    for i in range(1, n):
+        T[:, i + 1] = 2 * r_k * T[:, i] - T[:, i - 1]
 
-for i in range(1, n):
-    T[:, i + 1] = 2 * r_k * T[:, i] - T[:, i - 1]
+    # or numpy's routine
+    # T = np.polynomial.chebyshev.chebvander(r_k,n)
 
-# or numpy's routine
-# T = np.polynomial.chebyshev.chebvander(r_k,n)
+    parameters = [[float(x) for x in d.strip().split(',')[1:]] for d in open('paras.csv').readlines()]
+    for NN in range(4, 5):
+        paras = np.array(parameters[NN])
+        x_min = -10
+        x_max = 10
+        x_grid = np.linspace(x_min, x_max, 1000)
 
-# calculate the Chebyshev coefficients
-x_k = scale_up(r_k, x_min, x_max)
-y_k = zero_temperature_Green_function(x_k, paras)
-alpha = np.linalg.inv(T.T @ T) @ T.T @ y_k
+    # calculate the Chebyshev coefficients
+    x_k = scale_up(r_k, x_min, x_max)
+    y_k = spectral_function_fermion(x_k, paras)
+    alpha = np.linalg.inv(T.T @ T) @ T.T @ y_k
 
-# Use coefficients to compute an approximation of $f(x)$ over the grid of $x$:
-T = np.zeros((len(x_grid), n + 1))
+    # Use coefficients to compute an approximation of $f(x)$ over the grid of $x$:
+    T = np.zeros((len(x_grid), n + 1))
 
-T[:, 0] = np.ones((len(x_grid), 1)).T
+    T[:, 0] = np.ones((len(x_grid), 1)).T
 
-z_grid = scale_down(x_grid, x_min, x_max)
+    z_grid = scale_down(x_grid, x_min, x_max)
 
-T[:, 1] = z_grid.T
+    T[:, 1] = z_grid.T
 
-for i in range(1, n):
-    T[:, i + 1] = 2 * z_grid * T[:, i] - T[:, i - 1]
+    for i in range(1, n):
+        T[:, i + 1] = 2 * z_grid * T[:, i] - T[:, i - 1]
 
-# compute approximation
-Tf = T @ alpha
-# make sure to use the scaled down grid inside the Chebyshev expansion
-plt.figure()
-plt.plot(x_grid, zero_temperature_Green_function(x_grid, paras))
-plt.plot(x_grid, Tf)
-plt.show()
-plt.close()
+    # compute approximation
+    Tf = T @ alpha
+    # make sure to use the scaled down grid inside the Chebyshev expansion
+    plt.figure()
+    plt.plot(x_grid, spectral_function_fermion(x_grid, paras))
+    plt.plot(x_grid, Tf)
+    plt.title(f'm={m},n={n}')
+    plt.show()
+    plt.close()
--- examples/notebooks/spectral_function_fermion.py
+++ examples/notebooks/spectral_function_fermion.py
@@ -46,115 +46,51 @@ class LHS(sp.linalg.LinearOperator):
         return self._z.conj() * v - self._H.dot(v, **self._kwargs)
 
 
-parameters = [[float(x) for x in d.strip().split(',')[1:]] for d in open('paras.csv').readlines()]
-print(parameters[2])
+def spectral_function_fermion(omegas, paras):
+    # config
+    L = 6  # system size
+    # L = 4  # system size
+    part = L // 2  # 根据对称性，进行参数缩减
+    # paras = np.array([9.0, 0.0, 2.0, 1.8, 4.0, 0.0, 0.2, 0.0])
 
-NN = 4
+    # paras = np.array([9.0, 0.0, 2.0, 1.8, 0.0, 0.2])
+    U = paras[0]
+    ef = paras[1]
+    eis_part = paras[2:2 + part]
+    hoppings_part = paras[2 + part:]
+    eis = np.concatenate((eis_part, -1 * eis_part))
+    hoppings = np.concatenate((hoppings_part, hoppings_part))
 
-L = 6  # system size
-# L = 4  # system size
-part = L // 2  # 根据对称性，进行参数缩减
-# paras = np.array([9.0, 0.0, 2.0, 1.8, 4.0, 0.0, 0.2, 0.0])
-paras = np.array(parameters[NN])
-# paras = np.array([9.0, 0.0, 2.0, 1.8, 0.0, 0.2])
-U = paras[0]
-ef = paras[1]
-eis_part = paras[2:2 + part]
-hoppings_part = paras[2 + part:]
-eis = np.concatenate((eis_part, -1 * eis_part))
-hoppings = np.concatenate((hoppings_part, hoppings_part))
+    # hop_to_xxx， hop_from_xxx都是正符号，同一个系数，在hamiltonian中已经定好了符号
+    hop_to_impurity = [[hoppings[i], 0, i + 1] for i in range(L)]
+    hop_from_impurity = [[hoppings[i], i + 1, 0] for i in range(L)]
+    pot = [[ef, 0]] + [[eis[i], i + 1] for i in range(L)]
+    interaction = [[U, 0, 0]]
+    # end config
 
-# hop_to_xxx， hop_from_xxx都是正符号，同一个系数，在hamiltonian中已经定好了符号
-hop_to_impurity = [[hoppings[i], 0, i + 1] for i in range(L)]
-hop_from_impurity = [[hoppings[i], i + 1, 0] for i in range(L)]
-pot = [[ef, 0]] + [[eis[i], i + 1] for i in range(L)]
-interaction = [[U, 0, 0]]
+    # 在符号上 都用‘-+’ 或者‘+-’，不可以掺杂
+    static = [
+        ['-+|', hop_from_impurity],
+        ['-+|', hop_to_impurity],
+        ['|-+', hop_from_impurity],
+        ['|-+', hop_to_impurity],
+        ['n|', pot],  # up on-site potention
+        ['|n', pot],  # down on-site potention
+        ['z|z', interaction]  # up-down interaction
+    ]
 
-# 在符号上 都用‘-+’ 或者‘+-’，不可以掺杂
-static = [
-    ['-+|', hop_from_impurity],
-    ['-+|', hop_to_impurity],
-    ['|-+', hop_from_impurity],
-    ['|-+', hop_to_impurity],
-    ['n|', pot],  # up on-site potention
-    ['|n', pot],  # down on-site potention
-    ['z|z', interaction]  # up-down interaction
-]
+    # spectral peaks broadening factor
+    eta = 0.9
 
-omegas = np.arange(-10, 10, 0.03)
-# spectral peaks broadening factor
-eta = 0.9
+    Green = np.zeros_like(omegas, dtype=np.complex128)
 
-Green = np.zeros_like(omegas, dtype=np.complex128)
+    cdagger_op = [["+|", [0], 1.0]]
+    c_op = [["-|", [0], 1.0]]
 
-cdagger_op = [["+|", [0], 1.0]]
-c_op = [["-|", [0], 1.0]]
-
-plt.plot(paras, '-.o')
-plt.ylim([-5, 10])
-plt.grid()
-
-# this is for (3, 4) basis
-occupancy = L + 1
-N_up = occupancy // 2
-N_down = occupancy - N_up
-
-dynamic = []
-no_checks = dict(check_pcon=False, check_symm=False, check_herm=False)
-
-# construct basis
-basis_GS = spinful_fermion_basis_general(N=occupancy, Nf=(N_up, N_down))
-
-H0 = hamiltonian(static, dynamic, basis=basis_GS, dtype=np.float64, **no_checks)
-# calculate ground state
-[E0], GS = H0.eigsh(k=1, which="SA")
-
-# 产生算符，会导致电子增加，所以要加1
-basis_H1 = spinful_fermion_basis_general(N=occupancy, Nf=(N_up + 1, N_down))
-H1 = hamiltonian(static, [], basis=basis_H1, dtype=np.complex128, **no_checks)
-
-# shift sectors, |A> = c^\dagger |GS>
-psiA = basis_H1.Op_shift_sector(basis_GS, cdagger_op, GS)
-# solve (z-H)|x> = |A> solve for |x>  using iterative solver for each omega
-#   |x> = (z+E0-H)^-1 c^\dagger |GS>
-for i, omega in enumerate(omegas):
-    lhs = LHS(H1, omega, eta, E0)
-    x, exitCode = sp.linalg.bicg(lhs, psiA)
-    assert exitCode == 0
-    np.allclose(lhs._matvec(x), psiA)
-    Green[i] += -np.vdot(psiA, x) / np.pi
-
-# 湮灭算符，会导致电子减少，所以要减1
-basis_H2 = spinful_fermion_basis_general(N=occupancy, Nf=(N_up - 1, N_down))
-H2 = hamiltonian(static, [], basis=basis_H2, dtype=np.complex128, **no_checks)
-
-# shift sectors, |A> = c |GS>
-psiA = basis_H2.Op_shift_sector(basis_GS, c_op, GS)
-# solve (z-H)|x> = |A> solve for |x>  using iterative solver for each omega
-#   |x> = (z-E0+H)^-1 c |GS>
-for i, omega in enumerate(omegas):
-    lhs = LHS(H2, omega, eta, E0, isparticle=False)
-    x, exitCode = sp.linalg.bicg(lhs, psiA)
-    assert exitCode == 0
-    np.allclose(lhs._matvec(x), psiA)
-    Green[i] += -np.vdot(psiA, x) / np.pi
-
-plt.plot(omegas, Green[:].imag)
-plt.xlabel('$\\omega$')
-plt.ylabel('$Gpm.imag$')
-plt.title('$G_{+-}(\\omega)$')
-plt.ylim([0, 0.45])
-plt.xlim([-25, 25])
-plt.grid()
-plt.show()
-
-# this is for (4, 3) basis
-occupancy = L + 1
-
-# if 2 *(occupancy//2) != occupancy:
-if 0:
-    N_down = occupancy // 2
-    N_up = occupancy - N_down
+    # this is for (3, 4) basis
+    occupancy = L + 1
+    N_up = occupancy // 2
+    N_down = occupancy - N_up
 
     dynamic = []
     no_checks = dict(check_pcon=False, check_symm=False, check_herm=False)
@@ -185,7 +121,7 @@ if 0:
     basis_H2 = spinful_fermion_basis_general(N=occupancy, Nf=(N_up - 1, N_down))
     H2 = hamiltonian(static, [], basis=basis_H2, dtype=np.complex128, **no_checks)
 
-    # shift sectors, |A> = c^ |GS>
+    # shift sectors, |A> = c |GS>
     psiA = basis_H2.Op_shift_sector(basis_GS, c_op, GS)
     # solve (z-H)|x> = |A> solve for |x>  using iterative solver for each omega
     #   |x> = (z-E0+H)^-1 c |GS>
@@ -196,16 +132,75 @@ if 0:
         np.allclose(lhs._matvec(x), psiA)
         Green[i] += -np.vdot(psiA, x) / np.pi
 
-    Green *= 0.5
+    # this is for (4, 3) basis
+    occupancy = L + 1
 
-area = (omegas[1] - omegas[0]) * Green[:].imag.sum()
-print("area: ", area)
+    # if 2 *(occupancy//2) != occupancy:
+    if 0:
+        N_down = occupancy // 2
+        N_up = occupancy - N_down
 
-plt.plot(omegas, Green[:].imag)
-plt.xlabel('$\\omega$')
-plt.ylabel('$Gpm.imag$')
-plt.title('$G_{+-}(\\omega)$')
-plt.ylim([0, 0.45])
-plt.xlim([-25, 25])
-plt.grid()
-plt.show()
+        dynamic = []
+        no_checks = dict(check_pcon=False, check_symm=False, check_herm=False)
+
+        # construct basis
+        basis_GS = spinful_fermion_basis_general(N=occupancy, Nf=(N_up, N_down))
+
+        H0 = hamiltonian(static, dynamic, basis=basis_GS, dtype=np.float64, **no_checks)
+        # calculate ground state
+        [E0], GS = H0.eigsh(k=1, which="SA")
+
+        # 产生算符，会导致电子增加，所以要加1
+        basis_H1 = spinful_fermion_basis_general(N=occupancy, Nf=(N_up + 1, N_down))
+        H1 = hamiltonian(static, [], basis=basis_H1, dtype=np.complex128, **no_checks)
+
+        # shift sectors, |A> = c^\dagger |GS>
+        psiA = basis_H1.Op_shift_sector(basis_GS, cdagger_op, GS)
+        # solve (z-H)|x> = |A> solve for |x>  using iterative solver for each omega
+        #   |x> = (z+E0-H)^-1 c^\dagger |GS>
+        for i, omega in enumerate(omegas):
+            lhs = LHS(H1, omega, eta, E0)
+            x, exitCode = sp.linalg.bicg(lhs, psiA)
+            assert exitCode == 0
+            np.allclose(lhs._matvec(x), psiA)
+            Green[i] += -np.vdot(psiA, x) / np.pi
+
+        # 湮灭算符，会导致电子减少，所以要减1
+        basis_H2 = spinful_fermion_basis_general(N=occupancy, Nf=(N_up - 1, N_down))
+        H2 = hamiltonian(static, [], basis=basis_H2, dtype=np.complex128, **no_checks)
+
+        # shift sectors, |A> = c^ |GS>
+        psiA = basis_H2.Op_shift_sector(basis_GS, c_op, GS)
+        # solve (z-H)|x> = |A> solve for |x>  using iterative solver for each omega
+        #   |x> = (z-E0+H)^-1 c |GS>
+        for i, omega in enumerate(omegas):
+            lhs = LHS(H2, omega, eta, E0, isparticle=False)
+            x, exitCode = sp.linalg.bicg(lhs, psiA)
+            assert exitCode == 0
+            np.allclose(lhs._matvec(x), psiA)
+            Green[i] += -np.vdot(psiA, x) / np.pi
+
+        Green *= 0.5
+
+    area = (omegas[1] - omegas[0]) * Green[:].imag.sum()
+    print("area: ", area)
+    return Green[:].imag
+
+
+if __name__ == '__main__':
+    NN = 4
+    parameters = [[float(x) for x in d.strip().split(',')[1:]] for d in open('paras.csv').readlines()]
+    print(parameters[2])
+    paras = np.array(parameters[NN])
+
+    omegas = np.arange(-10, 10, 0.03)
+
+    spectral_values = spectral_function_fermion(omegas, paras)
+    plt.plot(omegas, spectral_values)
+    plt.xlabel('$\\omega$')
+    plt.ylabel('$Gpm.imag$')
+    plt.title('$G_{+-}(\\omega)$')
+    plt.ylim([0, 0.45])
+    plt.xlim([-25, 25])
+    plt.grid()
+    plt.show()
