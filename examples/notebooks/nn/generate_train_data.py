f83a871c1267b1e321f6399957b2d3e7c71d9ab7
zhh <zhenghuanhuan@zhejianglab.com>
2022-07-13 21:04:25 +0800

    train network

--- examples/notebooks/nn/generate_train_data.py
+++ examples/notebooks/nn/generate_train_data.py
@@ -1,21 +1,23 @@
+import os.path
+
 import pandas as pd
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 
 from examples.notebooks.spectral_function_fermion import spectral_function_fermion
 from examples.notebooks.chevbyshev_approximation_fermion_spectral_function import scale_up, scale_down
 
 
-def generate_paras(L, samples, n, train_file, **kwargs):
+def anderson_config(L, samples):
     part = L // 2
     names_anderson = ['U', 'ef'] + [f'eis_{i}' for i in range(part)] + [f'hoppings_{i}' for i in range(part)]
+
     # generate anderson config
     print('generate anderson config')
     anderson = []
     for i in range(samples):
         if i % 1000 == 0:
-            print(f"=== [{i+1}/{samples}] anderson parameters. ===")
+            print(f"=== [{i + 1}/{samples}] anderson parameters. ===")
         t_i = np.random.uniform(0.0, 1.5, part)
         epsilon_i = np.random.uniform(-5.0, 5.0, part)
         U = np.random.uniform(0, 10, 1)
@@ -27,7 +29,10 @@ def generate_paras(L, samples, n, train_file, **kwargs):
             plt.figure()
             plt.plot(data, 'ro')
             plt.show()
+    return names_anderson, anderson
 
+
+def polynomial(n, anderson):
     # get chebyshev polynomials
     m = n + 1  # the number of sample in chebyshev
     x_min = -25
@@ -39,7 +44,7 @@ def generate_paras(L, samples, n, train_file, **kwargs):
     total_alpha = []
     for i, paras in enumerate(anderson):
         if i % 20 == 0:
-            print(f"=== [{i+1}/{samples}] chebyshev polynomials. ===")
+            print(f"=== [{i + 1}/{samples}] chebyshev polynomials. ===")
         paras = np.array(paras)
         y_k = spectral_function_fermion(x_k, paras)
         # builds the Vandermonde matrix of Chebyshev polynomial expansion at the r_k nodes
@@ -51,6 +56,12 @@ def generate_paras(L, samples, n, train_file, **kwargs):
             plt.figure()
             plt.plot(np.arange(len(alpha)), alpha, 'ro', markersize=2)
             plt.show()
+    return names_alpha, total_alpha
+
+
+def generate_train_paras(L, samples, n, train_file, **kwargs):
+    names_anderson, anderson = anderson_config(L, samples)
+    names_alpha, total_alpha = polynomial(n, anderson)
 
     # save to csv file
     parameters = np.concatenate((anderson, total_alpha), axis=1)
@@ -61,19 +72,25 @@ def generate_paras(L, samples, n, train_file, **kwargs):
     return names
 
 
-# get
+def generate_test_paras(n, test_file, **kwargs):
+    df = pd.read_csv(test_file, index_col=0, header=None)
+    anderson = df.to_numpy()
+    names_alpha, total_alpha = polynomial(n, anderson)
+    df[names_alpha] = total_alpha
+    df.to_csv(test_file)
+    return
+
+
 if __name__ == '__main__':
     L = 7
     samples = 2000
     order_n = 256
     train_file = f'train_{samples}_{order_n}.csv'
     print(f"generate {train_file}...")
-    columns_name = generate_paras(L, samples, order_n, train_file)
-
+    # columns_name = generate_train_paras(L, samples, order_n, train_file)
+    test_path = '..'
+    test_file = os.path.join(test_path, 'paras.csv')
+    generate_test_paras(order_n, test_file=test_file)
     # read dataframe
-    df = pd.read_csv(train_file)
-    df.loc[1]
-    df.loc[1, "U"]
-    df.loc[0, ["poly_0", "poly_1"]]
-    df.iloc[0, 1]
-    df.loc[:, ["U", "ef"]]
+    df = pd.read_csv(test_file)
+    print(df.iloc[:5, :2].to_string())
--- examples/notebooks/nn/pytorch_dnn.py
+++ examples/notebooks/nn/pytorch_dnn.py
@@ -1,21 +1,27 @@
+import os.path
+
 import pandas as pd
+from json import load
 
 import torch
 import torch.nn as nn
-import pandas as pd
-from skimage import io, transform
 import numpy as np
 import matplotlib.pyplot as plt
-import torchvision
-from torchvision import transforms, utils
+from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
+
+from utils import scale_up, scale_down
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-n_b = 6  # in L = 6 Anderson model
-n = 256
-x_min = -25
-x_max = 25
+configure = 'config.json'
+with open(configure) as f:
+    config = load(f)
+    n_b = int(config['n_b'])
+    n = int(config['n'])
+    x_min = int(config['x_min'])
+    x_max = int(config['x_max'])
+
 names = "U,ef,eis_0,eis_1,eis_2,hoppings_0,hoppings_1,hoppings_2".strip().split(',') + ["poly_" + str(i) for i in
                                                                                         range(n + 1)]
 
@@ -23,14 +29,26 @@ input_size = n_b + 2
 no_hidden_units = [2 * input_size, 2 * input_size, 4 * input_size, 4 * input_size, 8 * input_size, 8 * input_size,
                    8 * input_size, 8 * input_size, 4 * input_size, 4 * input_size, 2 * input_size, 2 * input_size]
 num_classes = 1  # out for predict i-th order chebyshev polynomial
-num_epochs = 5
-batch_size = 2
+num_epochs = 50
+batch_size = 100
 learning_rate = 0.001
-orders = [0]  # 训练第几个系数
-sample_size = 2
+orders = [x for x in range(n + 1)]  # 训练第几个系数
+sample_size = 2000
 train_file = f'train_{sample_size}_{n}.csv'
-test_file = f'paras.csv'
+test_file = os.path.join('..', 'paras.csv')
 predict_file = f'predict_{input_size}_{n}.csv'
+
+
+def chebyshev(x_grid, alpha_c, alpha_n):
+    T = np.zeros((len(x_grid), n + 1))
+    T[:, 0] = np.ones((len(x_grid), 1)).T
+    z_grid = scale_down(x_grid, x_min, x_max)
+    T[:, 1] = z_grid.T
+    for i in range(1, n):
+        T[:, i + 1] = 2 * z_grid * T[:, i] - T[:, i - 1]
+    T_chebyshev = T @ alpha_c
+    T_nn = T @ alpha_n
+    return T_chebyshev, T_nn
 
 
 class AndersonChebyshevDataset(Dataset):
@@ -114,9 +132,11 @@ model = NeuralNet(input_size, no_hidden_units, num_classes).to(device)
 # loss and optimizer
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-#
+
 # Train the model
+print("=== step 1: training models ===")
 for order in orders:
+    print(f'=== training model_{order}.ckpt ===')
     # dataset
     train_dataset = AndersonChebyshevDataset(csv_file=train_file, order=order, transform=transforms.ToTensor())
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
@@ -124,7 +144,6 @@ for order in orders:
     total_step = len(train_dataloader)
     for epoch in range(num_epochs):
         for i, sample_batched in enumerate(train_dataloader):
-            print(f"{i} {sample_batched['anderson'].size()} {sample_batched['poly'].size()}")
             andersons = sample_batched['anderson'].reshape(-1, 8).to(device)
             polys = sample_batched['poly'].to(device).reshape(-1, 1)
 
@@ -140,43 +159,80 @@ for order in orders:
             if (i + 1) % 100 == 0:
                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch + 1, num_epochs, i + 1, total_step,
                                                                          loss.item()))
-    torch.save(model.state_dict(), f'model_{order}.ckpt')
+    model_path = os.path.join(f'model_{input_size}', f'model_{order}.ckpt')
+    torch.save(model.state_dict(), model_path)
 
-test_dataset = AndersonChebyshevDataset(csv_file=test_file, order=order, transform=transforms.ToTensor())
-test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
 
-# Test model
-df = pd.read_csv(test_file).iloc[:, [:input_size]]
-df.to_csv(predict_file)
+print("=== step 2: predict test data ===")
+# initialize predict data
+df = pd.read_csv(test_file, index_col=0)
+predict_df = df.iloc[:, :input_size]
+del df
+
 for order in orders:
+    test_dataset = AndersonChebyshevDataset(csv_file=test_file, order=order, transform=transforms.ToTensor())
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
     model = NeuralNet(input_size, no_hidden_units, num_classes)
-    model.load_state_dict(torch.load(f'model_{order}.ckpt'))
+    model_path = os.path.join(f'model_{input_size}', f'model_{order}.ckpt')
+    model.load_state_dict(torch.load(model_path))
     model.eval()
-    predict = []
+    predict = None
     with torch.no_grad():
         for sample_batched in test_dataloader:
             andersons = sample_batched['anderson'].reshape(-1, 8)
             poly = sample_batched['poly']
             outputs = model(andersons)
-            predict.extend(outputs)
+            outputs = outputs.numpy()
+            if predict is None:
+                predict = outputs
+            else:
+                predict = np.concatenate((predict, outputs), axis=0)
+    predict_df[f'poly_{order}'] = predict
+predict_df.to_csv(predict_file)
+print(predict_df.to_string())
 
-    df = pd.read_csv(predict_file)
-    df[f'poly_{order}'] = predict
-    df.to_csv(predict_file)
+print('=== step 3: plot results ===')
 
-print(df.to_string())
+alpha_chebyshev = pd.read_csv(test_file, index_col=0).to_numpy()[:, input_size:]
+alpha_nn = pd.read_csv(predict_file, index_col=0).to_numpy()[:, input_size:]
+assert alpha_chebyshev.shape[1] == n + 1
 
-# plot
-from utils import scale_up, scale_down
+plot_row = 8
+plot_col = 4
+plt.figure(14)
+fig_1, axs_1 = plt.subplots(plot_row, plot_col, figsize=(5 * plot_col, 2.5 * plot_row), facecolor='w',
+                            edgecolor='k')
+fig_1.subplots_adjust(hspace=0.5, wspace=0.5)
+axs_1 = axs_1.ravel()
+fig_1.suptitle("AW Chebyshev vs neural network")
 
-alpha_chebyshev_df = pd.read_csv(test_file)
+plt.figure(2)
+fig_2, axs_2 = plt.subplots(plot_row, plot_col, figsize=(5 * plot_col, 2.5 * plot_row), facecolor='w',
+                            edgecolor='k')
+fig_2.subplots_adjust(hspace=0.5, wspace=0.5)
+axs_2 = axs_2.ravel()
+fig_2.suptitle("Comparisons Chebyshev vs neural network")
 
-def chebyshev():
+for i, (alpha_c, alpha_n) in enumerate(zip(alpha_chebyshev, alpha_nn)):
     x_grid = np.linspace(x_min, x_max, 1000)
-    T = np.zeros((len(x_grid), n + 1))
-    T[:, 0] = np.ones((len(x_grid), 1)).T
-    z_grid = scale_down(x_grid, x_min, x_max)
-    T[:, 1] = z_grid.T
-    for i in range(1, n):
-        T[:, i + 1] = 2 * z_grid * T[:, i] - T[:, i - 1]
-        T_chebyshev = T @
+    T_c, T_n = chebyshev(x_grid, alpha_c, alpha_n)
+    plt.figure(14)
+    if i >= plot_row * plot_col:
+        break
+    axs_1[i].plot(x_grid, T_c)
+    axs_1[i].plot(x_grid, T_n)
+    axs_1[i].set_xlabel('$\\omega$')
+    axs_1[i].set_ylabel('spectral')
+    axs_1[i].set_xlim([-x_min, x_max])
+    axs_1[i].set_ylim([0, 0.45])
+
+# samplers are 1000 in paper experiments
+sampler = len(alpha_chebyshev)
+for i in range(plot_row * plot_col):
+    plt.figure(2)
+    x = alpha_chebyshev[:, i]
+    y = alpha_nn[:, i]
+    axs_2[i].plot(x, y, 'ro')
+    axs_2[i].set_xlabel('Neural Network coefficient')
+    axs_2[i].set_ylabel('Exact coefficient')
+plt.show()
